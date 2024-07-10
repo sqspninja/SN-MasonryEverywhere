2b32c8b643e7b7347b5b7843a3e5efa189f68970
SQSPNinja <bergendesignco@gmail.com>
2024-07-10 17:53:47 -0400

    added breakpoints and other options

--- sn-masonryeverywhere.js
+++ sn-masonryeverywhere.js
@@ -45,7 +45,21 @@
             if (document.querySelector(selector)) {
                 clearInterval(interval);
                 clearTimeout(timeout);
-                FlexMasonry.init(selector);
+                FlexMasonry.init(selector, {
+                    responsive: true,
+                    breakpointCols: {
+                        'min-width: 1500px': 6,
+                        'min-width: 1200px': 5,
+                        'min-width: 992px': 4,
+                        'min-width: 768px': 3,
+                        'min-width: 576px': 2,
+                    },
+                    /*
+                     * If `responsive` is `false`, this number of columns will always be shown,
+                     * no matter the width of the screen.
+                     */
+                    numCols: 4,
+                });
             }
         }, 100); // Check every 100 milliseconds
 
