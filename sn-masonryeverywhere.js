cc1841cb68476e059658d90c099c17b4be51b6ff
SQSPNinja <bergendesignco@gmail.com>
2024-07-10 17:57:26 -0400

    breakpoint defaults

--- sn-masonryeverywhere.js
+++ sn-masonryeverywhere.js
@@ -48,17 +48,11 @@
                 FlexMasonry.init(selector, {
                     responsive: true,
                     breakpointCols: {
-                        'min-width: 1500px': 6,
-                        'min-width: 1200px': 5,
-                        'min-width: 992px': 4,
-                        'min-width: 768px': 3,
-                        'min-width: 576px': 2,
-                    },
-                    /*
-                     * If `responsive` is `false`, this number of columns will always be shown,
-                     * no matter the width of the screen.
-                     */
-                    numCols: 4,
+                        'min-width: 1200px': 5, // Desktop
+                        'min-width: 992px': 4, // Laptop
+                        'min-width: 768px': 3, // Tablet
+                        'min-width: 576px': 1 // Mobile
+                    }
                 });
             }
         }, 100); // Check every 100 milliseconds
